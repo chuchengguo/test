8740ff046d26bcb5a4fafcd7673563744aa5b632
chuchengguo <976203593@qq.com>
2021-05-11 16:16:24 +0800

    git command

---
+++ 3.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+	cout << "git status" << endl;
+
+	cout << "git add  XXX" << endl;
+
+	cout << "git status" << endl;
+
+	cout << "git commit -m description" << endl;
+
+	cout << "git push" << endl;
+	return 0;
+}
